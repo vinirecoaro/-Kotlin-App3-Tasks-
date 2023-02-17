5bae0d097066bf6ffccda60665ababe206252c08
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-16 22:15:19 -0300

    Salvando dados do usuario com SharedPreferences

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -5,14 +5,17 @@ import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.constants.TaskConstants
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
+import com.devmasterteam.tasks.service.repository.SecurityPreferences
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
+    private val securityPreferences = SecurityPreferences(application.applicationContext)
 
     private val _login = MutableLiveData<ValidationModel>()
     val login: LiveData<ValidationModel> = _login
@@ -24,6 +27,9 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     fun doLogin(email: String, password: String) {
         personRepository.login(email, password, object : APIListener<PersonModel> {
             override fun onSucess(result: PersonModel) {
+                securityPreferences.store(TaskConstants.SHARED.TOKEN_KEY, result.token)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_KEY, result.personKey)
+                securityPreferences.store(TaskConstants.SHARED.PERSON_NAME, result.name)
                 _login.value = ValidationModel()
             }
             override fun onFailure(message: String) {
