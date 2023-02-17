4907d7e2a0484b899bf40e83c892f216978749af
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-16 22:10:16 -0300

    Implementacao do login - completo

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -1,14 +1,22 @@
 package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
+import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PersonModel
+import com.devmasterteam.tasks.service.model.ValidationModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val personRepository = PersonRepository(application.applicationContext)
+
+    private val _login = MutableLiveData<ValidationModel>()
+    val login: LiveData<ValidationModel> = _login
+
 
     /**
      * Faz login usando API
@@ -16,11 +24,10 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
     fun doLogin(email: String, password: String) {
         personRepository.login(email, password, object : APIListener<PersonModel> {
             override fun onSucess(result: PersonModel) {
-                TODO("Not yet implemented")
+                _login.value = ValidationModel()
             }
-
             override fun onFailure(message: String) {
-                TODO("Not yet implemented")
+                _login.value = ValidationModel(message)
             }
         })
     }
