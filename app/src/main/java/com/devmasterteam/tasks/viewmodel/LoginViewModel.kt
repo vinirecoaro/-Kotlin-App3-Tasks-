89df50e1c042795a362f244e6f56fadcaea71887
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-16 21:45:22 -0300

    Criacao da APIListener para tratar respostas da API e finalizacao do repository

--- app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/LoginViewModel.kt
@@ -2,17 +2,27 @@ package com.devmasterteam.tasks.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.devmasterteam.tasks.service.listener.APIListener
+import com.devmasterteam.tasks.service.model.PersonModel
 import com.devmasterteam.tasks.service.repository.PersonRepository
 
 class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val personRepository = PersonRepository()
+    private val personRepository = PersonRepository(application.applicationContext)
 
     /**
      * Faz login usando API
      */
     fun doLogin(email: String, password: String) {
-        personRepository.login(email, password)
+        personRepository.login(email, password, object : APIListener<PersonModel> {
+            override fun onSucess(result: PersonModel) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onFailure(message: String) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     /**
