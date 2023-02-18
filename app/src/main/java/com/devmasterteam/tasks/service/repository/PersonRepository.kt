c307bd6210411241cb5d55b71a3200e0fa514cca
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-18 14:44:22 -0300

    Melhoria na abstracao do BaseRepository - remocao do handleEnqueue

--- app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/PersonRepository.kt
@@ -12,21 +12,12 @@ import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Response
 
-class PersonRepository(val context: Context): BaseRepository() {
+class PersonRepository(context: Context): BaseRepository(context) {
 
     private val remote = RetrofitClient.getService(PersonService::class.java)
 
     fun login(email: String, password: String, listener: APIListener<PersonModel>){
-        val call = remote.login(email, password)
-        call.enqueue(object : retrofit2.Callback<PersonModel> {
-            override fun onResponse(call: Call<PersonModel>, response: Response<PersonModel>) {
-                handleResponse(response, listener)
-            }
-
-            override fun onFailure(call: Call<PersonModel>, t: Throwable) {
-                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
-            }
-        })
+        executeCall(remote.login(email, password), listener)
     }
 
 }
