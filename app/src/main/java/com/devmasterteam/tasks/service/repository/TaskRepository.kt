5fdc55de8ef2ed19aafacfd78c407c57936651e0
Vinicius Recoaro <vrecoaro@hotmail.com>
2023-02-18 11:33:25 -0300

    Cadastro de tarefas - API

--- app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
+++ app/src/main/java/com/devmasterteam/tasks/service/repository/TaskRepository.kt
@@ -1,10 +1,40 @@
 package com.devmasterteam.tasks.service.repository
 
+import android.content.Context
+import com.devmasterteam.tasks.R
+import com.devmasterteam.tasks.service.constants.TaskConstants
+import com.devmasterteam.tasks.service.listener.APIListener
+import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.remote.RetrofitClient
 import com.devmasterteam.tasks.service.repository.remote.TaskService
+import com.google.gson.Gson
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class TaskRepository {
+class TaskRepository(val context: Context) {
 
     private val remote = RetrofitClient.getService(TaskService::class.java)
+
+    fun create(task: TaskModel, listener: APIListener<Boolean>){
+        val call = remote.create(task.priorityId, task.description, task.dueDate, task.complete)
+        call.enqueue(object : Callback<Boolean>{
+            override fun onResponse(call: Call<Boolean>, response: Response<Boolean>) {
+                if(response.code() == TaskConstants.HTTP.SUCCESS){
+                    response.body()?.let { listener.onSucess(it) }
+                }else{
+                    listener.onFailure(failResponse(response.errorBody()!!.string()))
+                }
+            }
+
+            override fun onFailure(call: Call<Boolean>, t: Throwable) {
+                listener.onFailure(context.getString(R.string.ERROR_UNEXPECTED))
+            }
+        })
+    }
+
+    private fun failResponse(str: String): String{
+        return Gson().fromJson(str, String::class.java)
+    }
 
 }
--- app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
+++ app/src/main/java/com/devmasterteam/tasks/viewmodel/TaskFormViewModel.kt
@@ -4,18 +4,31 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.devmasterteam.tasks.service.listener.APIListener
 import com.devmasterteam.tasks.service.model.PriorityModel
 import com.devmasterteam.tasks.service.model.TaskModel
 import com.devmasterteam.tasks.service.repository.PriorityRepository
+import com.devmasterteam.tasks.service.repository.TaskRepository
 
 class TaskFormViewModel(application: Application) : AndroidViewModel(application) {
 
     private val priorityRepository = PriorityRepository(application.applicationContext)
+    private val taskRepository = TaskRepository(application.applicationContext)
 
     private val _priorityList = MutableLiveData<List<PriorityModel>>()
     val priorityList: LiveData<List<PriorityModel>> = _priorityList
 
-    fun save(task: TaskModel){}
+    fun save(task: TaskModel){
+        taskRepository.create(task, object : APIListener<Boolean>{
+            override fun onSucess(result: Boolean) {
+                val s = ""
+            }
+
+            override fun onFailure(message: String) {
+                val s = ""
+            }
+        } )
+    }
 
     fun loadPriorities(){
         _priorityList.value = priorityRepository.list()
